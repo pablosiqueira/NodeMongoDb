620855a1d36700cf1bf7eb4d5eba2dc443f0a82a
pablosiqueira <pablo16@outlook.com>
2022-04-26 17:15:30 -0300

    port and command update

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
-  app.listen(3000, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log('Example app listening on port 3000!')
     //app.use(homeController.getHome);
   });
